35a526385c82e0ff4676ba46393bfca2a351f637
LcsAlmeidaS <lcshenriquegs@gmail.com>
2025-09-03 16:25:05 -0300

    Potência.

---
+++ Exercicio01_Potencia/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+internal class Program
+{
+    static int Potencia(int a, int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return a * Potencia(a, n - 1);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Informe o valor de a: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Informe o valor de n: ");
+        int n = int.Parse(Console.ReadLine());
+        int resultado = Potencia(a, n);
+        Console.Write(resultado);
+        Console.ReadKey();
+    }
+}
