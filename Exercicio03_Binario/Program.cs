2f7bd07b8ec12588ceaf4335df0e25cd3ed0e287
LcsAlmeidaS <lcshenriquegs@gmail.com>
2025-09-03 16:27:27 -0300

    Conversor de decimal para binário.

---
+++ Exercicio03_Binario/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+internal class Program
+{
+    static void Binario(int num)
+    {
+        if (num == 0)
+            return;
+
+        Binario(num / 2);
+
+        Console.Write(num % 2);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número inteiro: ");
+        int num = int.Parse(Console.ReadLine());
+        Binario(num);
+
+        Console.ReadKey();
+    }
+}
