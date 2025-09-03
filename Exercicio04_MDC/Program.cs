13e610e3e2cd22b4763ae58e8021ac421449340b
LcsAlmeidaS <lcshenriquegs@gmail.com>
2025-09-03 16:34:15 -0300

    Final commit.

--- Exercicio04_MDC/Program.cs
+++ Exercicio04_MDC/Program.cs
@@ -1,5 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Program
 {
+
     static int MDC(int x, int y)
     {
         if (y == 0)
