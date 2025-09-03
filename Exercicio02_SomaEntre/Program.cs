c74c7fbe4daac10fdf30c824f313fb67ea122834
LcsAlmeidaS <lcshenriquegs@gmail.com>
2025-09-03 16:26:24 -0300

    Soma Entre 2 números.

---
+++ Exercicio02_SomaEntre/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+internal class Program
+{
+    static int SomaEntre(int m, int n)
+    {
+        if (n == m)
+            return n;
+
+        int soma = m + SomaEntre(m + 1, n);
+        return soma;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Valor de m: ");
+        int m = int.Parse(Console.ReadLine());
+        Console.Write("Valor de n: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Soma entre {m} e {n}: {SomaEntre(m, n)}");
+        Console.ReadKey();
+    }
+}
