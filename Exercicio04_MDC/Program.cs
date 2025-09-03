c7a5da1431948ee880165d46db874c7a598e5187
LcsAlmeidaS <lcshenriquegs@gmail.com>
2025-09-03 16:28:29 -0300

    Calcula MDC entre 2 números inteiros.

---
+++ Exercicio04_MDC/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+{
+    static int MDC(int x, int y)
+    {
+        if (y == 0)
+            return x;
+        return MDC(y, x % y);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("x: ");
+        int x = int.Parse(Console.ReadLine());
+        Console.Write("y: ");
+        int y = int.Parse(Console.ReadLine());
+        Console.WriteLine("MDC: " + MDC(x, y));
+        Console.ReadKey();
+    }
+}
